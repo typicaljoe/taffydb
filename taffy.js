8e2bb85c872110530db9d6c14a77fd6ef54f2e17
Jeff Rose <jefuri.rozu@gmail.com>
2013-06-27 10:03:13 -0400

    Updated TaffyDB to 2.7.1

--- taffy.js
+++ taffy.js
@@ -57,7 +57,7 @@
     // TC = Counter for Taffy DBs on page, used for unique IDs
     // cmax = size of charnumarray conversion cache
     // idpad = zeros to pad record IDs with
-    version = '2.7';
+    version = '2.7.1';
     TC      = 1;
     idpad   = '000000';
     cmax    = 1000;
