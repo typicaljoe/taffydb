45a1851e0b106f2e92117943497ce197d63e2f48
Daniel Ruf <DanielRuf@users.noreply.github.com>
2014-01-08 11:36:37 +0100

    Preserving Object prototypes
    https://github.com/tisztamo/taffydb/commit/63ce897df67fb68973b1749a7a025fc0b6aa0239

--- taffy.js
+++ taffy.js
@@ -1362,7 +1362,7 @@ var TAFFY, T,
 
             }
             else if ( T.isObject( v ) && settings.forcePropertyCase ){
-              o = {};
+              o = (typeof v == 'object') ? Object.create(Object.getPrototypeOf(v)) : {};
 
               eachin( v, function ( av, ai ) {
                 o[(settings.forcePropertyCase === 'lower') ? ai.toLowerCase()
@@ -1807,7 +1807,7 @@ var TAFFY, T,
     // *
     // ****************************************   
     TAFFY.mergeObj = function ( ob1, ob2 ) {
-      var c = {};
+      var c = Object.create(Object.getPrototypeOf(ob1));
       eachin( ob1, function ( v, n ) { c[n] = ob1[n]; });
       eachin( ob2, function ( v, n ) { c[n] = ob2[n]; });
       return c;
